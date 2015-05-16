fd03b270cb11b78f609d0bf3c525207ee607418d
Sem Derksen <semderksen@gmail.com>
2015-05-17 00:10:04 +0200

    More setting up
    Got to episode 4 for making my first mod.

---
+++ src/main/java/com/skiesarered/myfirstmod/MyFirstMod.java
@@ -0,0 +1,31 @@
+package com.skiesarered.myfirstmod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="MyFirstMod", name="My First Mod", version="1.7.10-0.1")
+public class MyFirstMod {
+
+    @Mod.Instance("MyFirstMod")
+    public static MyFirstMod instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
